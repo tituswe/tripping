4d2ffa78c9e9eb2470b97e5a0cffab211454999b
tituswe <tituslowesg@gmail.com>
2025-01-07 13:10:32 +0800

    Remove old files

--- src/app/(features)/dashboard/page.tsx
+++ src/app/(features)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { getTrips } from "@/actions/actions";
-import { Dashboard } from "@/components/dashboard/dashboard";
+import { Dashboard } from "./components/dashboard";
 
 export default async function DashboardPage() {
 	const trips = await getTrips();
